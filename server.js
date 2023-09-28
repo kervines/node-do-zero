28fe965f27d4023540173bcce02fd07ffd898a33
kervines <kervinzero@gmail.com>
2023-09-27 22:16:28 -0300

    implementa banco de dados e servidor

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import { fastify } from 'fastify';
-import { DatabaseMemory } from './database-memory.js';
+// import { DatabaseMemory } from './database-memory.js';
+import { DatabasePostgres } from './database-postgres.js';
 
 const server = fastify();
-const database = new DatabaseMemory();
+// const database = new DatabaseMemory();
+const database = new DatabasePostgres();
 
-server.post('/videos', (request, reply) => {
+server.post('/videos', async (request, reply) => {
   const { title, description, duration } = request.body;
-  database.create({
+  await database.create({
     title,
     description,
     duration,
@@ -14,19 +16,19 @@ server.post('/videos', (request, reply) => {
   return reply.status(201).send();
 });
 
-server.get('/videos', (request) => {
+server.get('/videos', async (request) => {
   const search = request.query.search;
 
-  const videos = database.list(search);
+  const videos = await database.list(search);
 
   return videos;
 });
 
-server.put('/videos/:id', (request, reply) => {
+server.put('/videos/:id', async (request, reply) => {
   const videoId = request.params.id;
   const { title, description, duration } = request.body;
 
-  database.update(videoId, {
+  await database.update(videoId, {
     title,
     description,
     duration,
